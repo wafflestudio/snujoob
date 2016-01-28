f4d9ed2f71c171aad5d2456aec474a4bef3e9aa8
Leeingnyo <dyd1928@naver.com>
2016-01-29 00:58:16 +0900

    change color, show progress bar

--- app/src/main/java/me/leeingnyo/snujoob/MainActivity.java
+++ app/src/main/java/me/leeingnyo/snujoob/MainActivity.java
@@ -1,5 +1,6 @@
 package me.leeingnyo.snujoob;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +25,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        progressBar = (LinearLayout)findViewById(R.id.progress_bar);
+
+
+        progressBar.setVisibility(View.VISIBLE);
     }
 
     @Override
